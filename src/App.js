565895c95a2c6f23a4853ab5f561888c40cd71fa
maitri-97 <mait.ch1997@gmail.com>
2024-12-21 21:09:06 +0000

    landing page initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Dashboard } from "./pages/dashboard";
+import { Stackholders } from "./pages/stackholders";
+import { Landing } from "./pages/landing";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import "./assets/css/style.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" exact element={<Landing />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/stackholders" element={<Stackholders />} />
+          </Routes>
+        </main>
+      </Router>
+    </>
   );
 }
 
